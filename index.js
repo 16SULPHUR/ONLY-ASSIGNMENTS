8d74a0b819f24e5d6daad543cf8ffc540457dc3f
Ankit Patil <22amtics124@gmail.com>
2023-04-27 00:19:00 +0530

    created index html , css , js

---
+++ index.js
@@ -0,0 +1,15 @@
+dropdownlist = document.getElementById("dropdown-list")
+dropdownlist.style.display = "none"
+
+function showdropdown() {
+    if (dropdownlist.style.display == "none") {
+        dropdownlist.style.display = "flex";
+    }
+    else {
+        dropdownlist.style.display = "none";
+    }
+}
+
+function disnone() {
+     dropdownlist.style.display = "none";
+}
