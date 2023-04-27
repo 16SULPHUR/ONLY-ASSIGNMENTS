cfcc7233395c8825cf917f13c85da7725230b113
Ankit Patil <22amtics124@gmail.com>
2023-04-27 22:45:52 +0530

    added scroll to top button

--- main-template.js
+++ main-template.js
@@ -12,3 +12,24 @@ function showdropdown() {
 function disnone() {
   dropdownlist.style.display = "none";
 }
+
+// --------------------scroll - top--------------------
+// Get the button
+let mybutton = document.getElementById("scroll-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
